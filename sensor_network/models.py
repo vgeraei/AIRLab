325162157d26e77f071ef7ee2bddab8244c4d326
vgeraei <vahidgn@gmail.com>
2016-11-19 16:28:24 +0330

    sensor models

--- sensor_network/models.py
+++ sensor_network/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Sensor(models.Model):
+    data = models.CharField(max_length=10, null=True, blank=True, default=0)
+
+    Sensors = (
+        (Tempreture, 'TMP'),
+        (Humidity, 'HUM'),
+        (Door_status, 'DST'),
+        (Window_status, 'WST'),
+        (PIR, 'PIR'),
+        (Luminance,'LUM')
+    )
+    name = models.CharField(max_length=10, choices=Sensors)
+
+    time = models.DateTimeField(auto_now_add=True, blank=True)
