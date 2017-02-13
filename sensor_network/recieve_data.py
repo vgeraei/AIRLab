0e5525cc9b1c99c4be1729676e0a6fba20a3e26d
vgeraei <vahidgn@gmail.com>
2017-02-13 17:44:17 +0330

    rec test

--- sensor_network/recieve_data.py
+++ sensor_network/recieve_data.py
@@ -176,10 +176,10 @@ def resp_put(q):
             # q.put(response)
             print(response)
             if light_var:
-                zb.send('tx', frame_id='\x01',  dest_addr_long='\x00\x13\xa2\x00\x40@\xc8\xe5\x22', data='1')
+                zb.send('tx', frame_id='\x01',  dest_addr_long='\x00\x13\xa2\x00\x40\xc8\xe5\x22', data='1')
                 light_var = 0
             else:
-                zb.send('tx', frame_id='\x01', dest_addr_long='\x00\x13\xa2\x00\x40@\xc8\xe5\x22', data='0')
+                zb.send('tx', frame_id='\x01', dest_addr_long='\x00\x13\xa2\x00\x40\xc8\xe5\x22', data='0')
                 light_var = 1
 
             # time.sleep(5)
